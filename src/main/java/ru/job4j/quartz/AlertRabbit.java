f1ecac0bb9838e7e988bd32097a7236040b9115c
IvanM13th <bmchek13@gmail.com>
2023-01-16 12:03:52 +0500

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,45 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        Properties prop = getProperties();
+        int interval = Integer.parseInt(prop.getProperty("rabbit.interval"));
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = JobBuilder.newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = SimpleScheduleBuilder.simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .startNow().withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    private static Properties getProperties() {
+        Properties config = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(in);
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return config;
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
