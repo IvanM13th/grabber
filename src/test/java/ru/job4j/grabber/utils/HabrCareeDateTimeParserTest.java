b57a99c5943df94d210484e989ca73f9098e17e7
IvanM13th <bmchek13@gmail.com>
2023-01-17 10:23:08 +0500

    2.1. Преобразование даты [#289476]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareeDateTimeParserTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HabrCareeDateTimeParserTest {
+
+    @Test
+    public void whenParseTextToDate() {
+        String dateText = "2023-01-16T17:01:18+03:00";
+        LocalDateTime time = new HabrCareeDateTimeParser().parse(dateText);
+        assertThat(time).isInstanceOf(LocalDateTime.class);
+        assertThat(time).isEqualTo(LocalDateTime.parse(dateText, DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+    }
+}
+
